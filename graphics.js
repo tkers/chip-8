1c53dedf36b34fb24e72f09f397d5d1d3d5a3975
Tijn Kersjes <tijn@divbyzero.nl>
2018-04-10 19:44:38 +0200

    Render screen on startup

--- graphics.js
+++ graphics.js
@@ -10,14 +10,18 @@ function initScreen(canvas) {
   canvas.width = width * cellSize
   canvas.height = height * cellSize
 
-  return function renderScreen(gfx) {
+  const renderScreen = gfx => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         const pix = gfx[x + width * y]
-        ctx.fillStyle = [bgColor, fgColor][pix]
+        ctx.fillStyle = pix ? fgColor : bgColor
         ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
       }
     }
   }
+
+  renderScreen([])
+
+  return renderScreen
 }

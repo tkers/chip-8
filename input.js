fba025119af5f6ac16add93a6cc863067e818bad
Tijn Kersjes <tijn@divbyzero.nl>
2018-04-10 18:25:11 +0200

    Remap keys to resemble classic layout

--- input.js
+++ input.js
@@ -1,7 +1,20 @@
 function initInput(handlePress, handleRelease) {
 
-  const getKeyIndex = event => parseInt(event.key, 16)
-  const isValidKeyIndex = index => !isNaN(index) && index >= 0 && index <= 16
+  /*
+    1 2 3 C
+    4 5 6 D
+    7 8 9 E
+    A 0 B F
+  */
+  const keyMap = {
+    '1': 0x1, '2': 0x2, '3': 0x3, '4': 0xc,
+    'q': 0x4, 'w': 0x5, 'e': 0x6, 'r': 0xd,
+    'a': 0x7, 's': 0x8, 'd': 0x9, 'f': 0xe,
+    'z': 0xa, 'x': 0x0, 'c': 0xb, 'v': 0xf
+  }
+
+  const getKeyIndex = event => keyMap[event.key]
+  const isValidKeyIndex = index => typeof index !== 'undefined'
 
   document.addEventListener('keydown', event => {
     const index = getKeyIndex(event)

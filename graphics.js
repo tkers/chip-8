49d8e7c3992d2bd56b0202b39eda2703e42e9af1
Tijn Kersjes <tijn@reaktor.fi>
2018-04-16 17:52:53 +0200

    Improved canvas pixel scaling

--- graphics.js
+++ graphics.js
@@ -1,14 +1,13 @@
 function initScreen(canvas) {
   const width = 64
   const height = 32
-  const cellSize = 10
   const bgColor = '#B6C6C4'
   const fgColor = '#222825'
 
   const ctx = canvas.getContext('2d')
 
-  canvas.width = width * cellSize
-  canvas.height = height * cellSize
+  canvas.width = width
+  canvas.height = height
 
   const renderScreen = gfx => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -16,7 +15,7 @@ function initScreen(canvas) {
       for (let x = 0; x < width; x++) {
         const pix = gfx[x + width * y]
         ctx.fillStyle = pix ? fgColor : bgColor
-        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
+        ctx.fillRect(x, y, 1, 1)
       }
     }
   }

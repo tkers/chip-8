553414aa36aa6f76bf94f1e14ae9c93ddfb70d52
Tijn Kersjes <tijn@reaktor.fi>
2018-04-18 22:59:12 +0300

    Improve rendering with canvas image data

--- graphics.js
+++ graphics.js
@@ -1,23 +1,25 @@
 function initScreen(canvas) {
   const width = 64
   const height = 32
-  const bgColor = '#B6C6C4'
-  const fgColor = '#222825'
+
+  const bgColor = 0xFFC4C6B6
+  const fgColor = 0xFF252822
+  const palette = [bgColor, fgColor]
 
   const ctx = canvas.getContext('2d')
 
   canvas.width = width
   canvas.height = height
 
+  const imageData = new ImageData(width, height)
+  const buff = new Uint32Array(imageData.data.buffer)
+
   const renderScreen = gfx => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        const pix = gfx[x + width * y]
-        ctx.fillStyle = pix ? fgColor : bgColor
-        ctx.fillRect(x, y, 1, 1)
-      }
-    }
+   const len = gfx.length
+   for (let i = 0; i < len; i++) {
+     buff[i] = palette[gfx[i]]
+   }
+    ctx.putImageData(imageData, 0, 0)
   }
 
   renderScreen([])
